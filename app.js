fcea195b9a9465061e800818ec8cd8ac0ace07ba
Ariel Blasi <exorcismo@gmail.com>
2018-05-08 17:54:08 -0300

    Add tasks items

--- app.js
+++ app.js
@@ -7,15 +7,33 @@ const taskInput = document.querySelector('#task')
 
 // Load all event listeners
 loadEventListeners()
-const loadEventListeners = () => {
+
+function loadEventListeners() {
 	//Add task event
 	form.addEventListener('submit', addTask)
 }
 
 //Add Task, takes event since it's event handler
-const addTask = (e) {
-	e.preventDefault(); // Prevent form submit
+function addTask(e) {
+	e.preventDefault() // Prevent form submit
 	if (taskInput.value === '') {
-		alert('Add a task')
+		console.log('Please add a task')
 	}
+	//Create li element
+	const li = document.createElement('li')
+	li.className = 'collection-item' //Add class
+	//Create text node and append to li
+	li.appendChild(document.createTextNode(taskInput.value))
+	//Create new link element
+	const link = document.createElement('a')
+	//Add class, and move to the right with secondary content
+	link.className = 'delete-item secondary-content'
+	//Add icon html
+	link.innerHTML = '<i class="material-icons">delete_forever</i>'
+	//Append the link to li
+	li.appendChild(link)
+	//Append the li to ul
+	taskList.appendChild(li)
+	//Clear the input
+	taskInput.value = ''
 }
