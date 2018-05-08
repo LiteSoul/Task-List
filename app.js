c92c7c80e65feedeacd6f267c28cdcd74c7f7eaf
Ariel Blasi <exorcismo@gmail.com>
2018-05-08 19:21:08 -0300

    remove

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const taskInput = document.querySelector('#task')
 loadEventListeners()
 
 function loadEventListeners() {
+	//DOM Load Event
+	document.addEventListener('DOMContentLoaded', getTasks)
 	//Add task event
 	form.addEventListener('submit', addTask)
 	//Remove task event
@@ -17,6 +19,33 @@ function loadEventListeners() {
 	clearBtn.addEventListener('click', clearTasks)
 	//Filter tasks
 	filter.addEventListener('keyup', filterTasks)
+}
+
+//Get tasks from LS
+function getTasks() {
+	let tasks
+	if (localStorage.getItem('tasks') === null) {
+		tasks = []
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'))
+	}
+	tasks.forEach(task => {
+		//Create li element
+		const li = document.createElement('li')
+		li.className = 'collection-item' //Add class
+		//Create text node and append to li
+		li.appendChild(document.createTextNode(task))
+		//Create new link element
+		const link = document.createElement('a')
+		//Add class, and move to the right with secondary content
+		link.className = 'delete-item secondary-content'
+		//Add icon html
+		link.innerHTML = '<i class="material-icons">delete_forever</i>'
+		//Append the link to li
+		li.appendChild(link)
+		//Append the li to ul
+		taskList.appendChild(li)
+	})
 }
 
 //Add Task, takes event since it's event handler
@@ -40,8 +69,22 @@ function addTask(e) {
 	li.appendChild(link)
 	//Append the li to ul
 	taskList.appendChild(li)
+	//Store in LocalStorage
+	storeTaskInLS(taskInput.value)
 	//Clear the input
 	taskInput.value = ''
+}
+
+//Store task
+function storeTaskInLS(task) {
+	let tasks
+	if (localStorage.getItem('tasks') === null) {
+		tasks = []
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'))
+	}
+	tasks.push(task)
+	localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 //Remove task
@@ -54,8 +97,28 @@ function removeTask(e) {
 	if (aTag.classList.contains('delete-item')) {
 		if (confirm(`Are you sure you want to delete this task?:\n${liTagText}`)) {
 			liTag.remove()
+			//Remove from LS
+			removeTaskFromLS(aTag.parentElement)
 		}
 	}
+}
+
+//Remove from LS
+function removeTaskFromLS(taskItem) {
+	let tasks
+	if (localStorage.getItem('tasks') === null) {
+		tasks = []
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'))
+	}
+
+	tasks.forEach((task, index) => {
+		if (taskItem.textContent === task) {
+			tasks.splice(index, 1)
+		}
+	})
+
+	localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 //Clear tasks
@@ -64,13 +127,20 @@ function clearTasks() {
 	while (taskList.firstChild) {
 		taskList.firstChild.remove()
 	}
+	//Clear from LS
+	clearTasksFromLS()
+}
+
+//Clear tasks from LS
+function clearTasksFromLS() {
+	localStorage.clear()
 }
 
 //Filter tasks
 function filterTasks(e) {
 	const text = e.target.value.toLowerCase()
 	const allTasks = document.querySelectorAll('.collection-item')
-	allTasks.forEach((task) => {
+	allTasks.forEach(task => {
 		const item = task.firstChild.textContent
 		if (item.toLowerCase().indexOf(text) != -1) {
 			task.style.display = 'block'
